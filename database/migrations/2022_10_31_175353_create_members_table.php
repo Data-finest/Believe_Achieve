a6dfe54f574018578968a263c1bbb01b8dcb1b71
kahmyl <kamilhabib556@gmail.com>
2022-11-09 21:08:13 +0100

    updated gitignore

--- database/migrations/2022_10_31_175353_create_members_table.php
+++ database/migrations/2022_10_31_175353_create_members_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->integer('membership_id');
+            $table->integer('membership_id')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('parents_name');
